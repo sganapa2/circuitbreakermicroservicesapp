0631c6f0801526c23728308ed2b28a4c16c7ba07
root <root@localhost.localdomain>
2020-07-20 14:00:15 +0530

    added files as initial commit

---
+++ src/main/java/com/my/circuitbreaker/circuitbreakerapp/CircuitbreakerappApplication.java
@@ -0,0 +1,13 @@
+package com.my.circuitbreaker.circuitbreakerapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitbreakerappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircuitbreakerappApplication.class, args);
+	}
+
+}
